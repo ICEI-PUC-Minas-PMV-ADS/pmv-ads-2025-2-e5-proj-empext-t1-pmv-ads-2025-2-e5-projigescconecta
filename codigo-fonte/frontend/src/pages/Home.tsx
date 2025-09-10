192888b073a948e1c5ba28c16cf1088398def361
Lucas Bebiano <lucasbebianolbx@gmail.com>
2025-09-10 20:24:11 -0300

    feat!: Arquitetura inicial do frontend

---
+++ codigo-fonte/frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Bem-vindo ao Igesc Conecta!</h1>
+      <p>Esta é a página inicial do seu projeto.</p>
+    </div>
+  );
+};
+
+export default Home;
