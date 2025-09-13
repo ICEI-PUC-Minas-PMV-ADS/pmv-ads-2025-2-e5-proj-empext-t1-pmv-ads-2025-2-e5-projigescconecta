a93af0c8411445fe1a81cc06708da2274c270c27
Fena <fe.oorschot@gmail.com>
2025-09-13 18:47:17 -0300

    feat: Criado end point para os serviços de token no login e refresh token

---
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Features/Auth/LoginUserEndpoint.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using IgescConecta.API.Services;
+using Microsoft.AspNetCore.Identity;
+using IgescConecta.Domain.Entities;
+
+namespace IgescConecta.API.Features.Auth
+{
+    [Route("/api/auth")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Auth")]
+    public class LoginUserEndpoint : ControllerBase
+    {
+        private readonly IAuthAuthenticatonService _auth;
+        private readonly UserManager<User> _userManager;
+        public LoginUserEndpoint(IAuthAuthenticatonService auth, UserManager<User> userManager)
+        {
+            _auth = auth;
+            _userManager = userManager;
+        }
+
+        [HttpPost("Login", Name = "Login")]
+        public async Task<ActionResult<UserToken>> LoginUser(UserLogin model)
+        {
+            var user = await _userManager.FindByEmailAsync(model.Email);
+
+            if(user != null && await _userManager.CheckPasswordAsync(user, model.Password))
+            {
+                return await _auth.BuildToken(user);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                return BadRequest(ModelState);
+            }
+        }
+    }
+}
