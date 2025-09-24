5c3019d596d48b6105028a198fb8aafd4eb76366
Fena <fe.oorschot@gmail.com>
2025-09-24 19:36:57 -0300

    feat: Criado método de auditoria e corrigido DbContext

--- codigo-fonte/IgescConecta.API/IgescConecta.API/Data/ApplicationDbContext.cs
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Data/ApplicationDbContext.cs
@@ -1,45 +1,123 @@
 ﻿using IgescConecta.Domain.Entities;
+using IgescConecta.Domain.Shared;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Security.Claims;
 
 namespace IgescConecta.API.Data
 {
     public class ApplicationDbContext : IdentityDbContext<User, Role, int>
     {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options, IHttpContextAccessor contextAccessor) : base(options)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
         public DbSet<User> Users { get; set; }
 
         public DbSet<Role> Roles { get; set; }
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-        }
+        public DbSet<Company> Companies { get; set; }
+
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Donation> Donations { get; set; }
+
+        public DbSet<OriginBusinessCase> OriginBusinessCases { get; set; }
+
+        public DbSet<Osc> Oscs { get; set; }
+
+        public DbSet<Beneficiaries> Beneficiaries { get; set; }
+
+        public DbSet<BusinessCase> BusinessCases { get; set; }
+
+        public DbSet<Person> Persons { get; set; }
+
+        public DbSet<PersonCompany> PersonCompanies { get; set; }
+
+        public DbSet<PersonOsc> PersonOscs { get; set; }
+
+        public DbSet<PersonTeam> PersonTeams { get; set; }
+
+        public DbSet<ProjectProgram> ProjectPrograms { get; set; }
+
+        public DbSet<ProjectDocument> ProjectDocuments { get; set; }
+
+        public DbSet<ProjectTheme> projectThemes { get; set; }
+
+        public DbSet<ProjectType> ProjectTypes { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
             builder.Entity<User>().ToTable("Users");
             builder.Entity<Role>().ToTable("Roles");
-            builder.Entity<Beneficiaries>().ToTable("Beneficiaries");
-            builder.Entity<BusinessCase>().ToTable("BusinessCases");
-            builder.Entity<Company>().ToTable("Companies");
-            builder.Entity<Donation>().ToTable("Donations");
-            builder.Entity<Osc>().ToTable("Oscs");
-            builder.Entity<Person>().ToTable("Persons");
-            builder.Entity<PersonCompany>().ToTable("PersonCompanies");
-            builder.Entity<PersonOsc>().ToTable("PersonOscs");
-            builder.Entity<PersonTeam>().ToTable("PersonTeams");
-            builder.Entity<ProjectDocument>().ToTable("ProjectDocuments");
-            builder.Entity<ProjectProgram>().ToTable("ProjectPrograms");
-            builder.Entity<ProjectTheme>().ToTable("ProjectThemes");
-            builder.Entity<ProjectType>().ToTable("ProjectTypes");
-            builder.Entity<Course>().ToTable("Courses");
             builder.Entity<IdentityUserClaim<int>>().ToTable("UserClaims");
             builder.Entity<IdentityUserLogin<int>>().ToTable("UserLogins");
             builder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
             builder.Entity<IdentityUserToken<int>>().ToTable("UserTokens");
             builder.Entity<IdentityRoleClaim<int>>().ToTable("RoleClaims");
             builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            OnBeforeSaving();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+        }
+
+        public override async Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            OnBeforeSaving();
+            return await base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        private void OnBeforeSaving()
+        {
+            var entities = ChangeTracker.Entries()
+                .Where(x => x.Entity is BaseEntity || x.Entity is User)
+                .ToList();
+            UpdateTimestamps(entities);
+        }
+
+        private void UpdateTimestamps(List<EntityEntry> entries)
+        {
+            var filtred = entries
+                .Where(x => x.State == EntityState.Added
+                || x.State == EntityState.Modified);
+
+            var currentUserId = Convert.ToInt32(_contextAccessor?.HttpContext?.User.FindFirst(ClaimTypes.NameIdentifier)?.Value);
+
+            foreach (var entry in filtred)
+            {
+                if(entry.Entity is User user)
+                {
+                    if(entry.State == EntityState.Added)
+                    {
+                        user.CreatedAt = DateTime.UtcNow;
+                        user.CreatedBy = currentUserId;
+                    }
+
+                    user.UpdatedAt = DateTime.UtcNow;
+                    user.UpdatedBy = currentUserId;
+                }
+                if(entry.Entity is BaseEntity entity)
+                {
+                    if(entry.State == EntityState.Modified)
+                    {
+                        entity.CreatedAt = DateTime.UtcNow;
+                        entity.CreatedBy = currentUserId;
+                    }
+
+                    entity.UpdatedAt = DateTime.UtcNow;
+                    entity.UpdatedBy = currentUserId;
+                }
+            }
         }
     }
 }
