4a956c9079340ad15fbc3504cdbd9a3775f3e525
Fena <fe.oorschot@gmail.com>
2025-09-29 20:19:36 -0300

    feat: Criado Crud para Causa

---
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Features/BusinessCases/DeleteBusinessCase/DeleteBusinessCaseEndpoint.cs
@@ -0,0 +1,26 @@
+﻿using IgescConecta.API.Common.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IgescConecta.API.Features.BusinessCases.DeleteBusinessCase
+{
+    [ApiAuthorize]
+    [Route("/api/businesscase")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "BusinessCases")]
+    public class DeleteBusinessCaseEndpoint : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public DeleteBusinessCaseEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpDelete("{businessCaseId}", Name = "DeleteBusinessCase")]
+        public async Task<ActionResult> DeleteBusinessCase(int businessCaseId)
+        {
+            var result = await _mediator.Send(new DeleteBusinessCaseCommand(businessCaseId));
+            return result.IsSuccess ? Ok() : BadRequest(result.Error);
+        }
+    }
+}
---
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Features/BusinessCases/GetBusinessCase/GetBusinessCaseQuery.cs
@@ -0,0 +1,41 @@
+﻿using IgescConecta.API.Common.Validation;
+using IgescConecta.API.Data;
+using IgescConecta.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace IgescConecta.API.Features.BusinessCases.GetBusinessCase
+{
+    public class GetBusinessCaseQuery : IRequest<Result<BusinessCase,ValidationFailed>>
+    {
+        public int BusinessCaseId { get; set; }
+
+        public GetBusinessCaseQuery(int id)
+        {
+            BusinessCaseId = id;
+        }
+    }
+
+    internal sealed class GetBusinessCaseQueryHandler : IRequestHandler<GetBusinessCaseQuery, Result<BusinessCase, ValidationFailed>>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GetBusinessCaseQueryHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Result<BusinessCase, ValidationFailed>> Handle(GetBusinessCaseQuery request, CancellationToken cancellationToken)
+        {
+            var businessCase = await _context.BusinessCases
+                .Where(bc => bc.Id == request.BusinessCaseId)
+                .Include(bc => bc.Origins)
+                .FirstOrDefaultAsync();
+
+            if (businessCase == null)
+                return new ValidationFailed(new[] { "Business Case not Found" });
+
+            return businessCase;
+        }
+    }
+}
