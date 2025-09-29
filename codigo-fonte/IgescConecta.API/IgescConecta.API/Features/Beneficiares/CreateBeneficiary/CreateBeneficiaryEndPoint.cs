bd7e5b7f982d04768582c8dd2aed7586ce74045f
Fena <fe.oorschot@gmail.com>
2025-09-29 20:18:29 -0300

    feat: Criado CRUD de público

---
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Features/Beneficiares/CreateBeneficiary/CreateBeneficiaryEndPoint.cs
@@ -0,0 +1,44 @@
+﻿using IgescConecta.API.Common.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IgescConecta.API.Features.Beneficiares.CreateBeneficiary
+{
+    [ApiAuthorize]
+    [Route("/api/beneficiary")]
+    [ApiController]
+    [ApiExplorerSettings(GroupName = "Beneficiaries")]
+    public class CreateBeneficiaryEndPoint : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CreateBeneficiaryEndPoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("CreateBeneficiary", Name = "CreateBeneficiary")]
+        public async Task<ActionResult<CreateBeneficiaryResponse>> CreateBeneficiary([FromBody] CreateBeneficiaryRequest request)
+        {
+            var result = await _mediator.Send(new CreateBeneficiaryCommand
+            {
+                Name = request.Name
+            });
+            return result.IsSuccess ? Ok(new CreateBeneficiaryResponse(result.Value)) : BadRequest(result.Error);
+        }
+    }
+
+    public class CreateBeneficiaryRequest
+    {
+        public string Name { get; set; }
+    }
+
+    public class CreateBeneficiaryResponse
+    {
+        public int BeneficiaryId { get; set; }
+        public CreateBeneficiaryResponse(int beneficiaryId)
+        {
+            BeneficiaryId = beneficiaryId;
+        }
+    }
+}
