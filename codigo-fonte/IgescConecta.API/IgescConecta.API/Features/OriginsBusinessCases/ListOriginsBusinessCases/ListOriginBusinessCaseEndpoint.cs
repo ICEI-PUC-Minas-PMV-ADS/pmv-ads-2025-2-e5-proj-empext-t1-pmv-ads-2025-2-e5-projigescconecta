ae86da0cd47325953b4d773430467ace400743e9
Fena <fe.oorschot@gmail.com>
2025-10-09 19:20:32 -0300

    fix: Ajustado end point de listar e obeter causa

--- codigo-fonte/IgescConecta.API/IgescConecta.API/Features/OriginsBusinessCases/ListOriginsBusinessCases/ListOriginBusinessCaseEndpoint.cs
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Features/OriginsBusinessCases/ListOriginsBusinessCases/ListOriginBusinessCaseEndpoint.cs
@@ -21,7 +21,7 @@ namespace IgescConecta.API.Features.OriginsBusinessCases.ListOriginsBusinessCase
         [HttpPost("ListOriginBusinessCase", Name = "ListOriginBusinessCase")]
         public async Task<ActionResult<ListOriginBusinessCaseViewModel>> ListOriginBusinessCase(ListOriginBusinessCaseRequest request)
         {
-            var result = await _mediator.Send(new ListOriginBusinessCaseQuery(request.PageNumber, request.PageSize, request.Filters));
+            var result = await _mediator.Send(new ListOriginBusinessCaseQuery(request.PageNumber, request.PageSize, request.Filters, request.BusinessCaseId));
             return Ok(result);
         }
     }
@@ -33,5 +33,7 @@ namespace IgescConecta.API.Features.OriginsBusinessCases.ListOriginsBusinessCase
         public int PageSize { get; set; }
 
         public List<Filter> Filters { get; set; } = [];
+
+        public int BusinessCaseId { get; set; }
     }
 }
