dea79be23ee5222e94968f84d81fd66d40b9c68b
Fena <fe.oorschot@gmail.com>
2025-10-26 21:27:42 -0300

    feat: Criado Crud para PersonOsc

---
+++ codigo-fonte/IgescConecta.API/IgescConecta.API/Features/PersonOscs/DeletePersonOsc/DeletePersonOscEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using IgescConecta.API.Common.Extensions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IgescConecta.API.Features.PersonOscs.DeletePersonOsc
+{
+    [ApiAuthorize]
+    [Route("/api/person-osc")]
+    [ApiAuthorize]
+    [ApiExplorerSettings(GroupName = "PersonOsc")]
+    public class DeletePersonOscEndpoint : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DeletePersonOscEndpoint(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpDelete("{PersonOscId}", Name = "DeletePersonOsc")]
+        public async Task<ActionResult> DeletePersonOsc(int personOscId)
+        {
+            var result = await _mediator.Send(new DeletePersonOscCommand(personOscId));
+            return result.IsSuccess ? Ok(result) : BadRequest(result.Error);
+        }
+    }
+}
